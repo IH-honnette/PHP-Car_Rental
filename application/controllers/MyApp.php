61e62a2ee45b108c1ba7e96e91927db8651a5e05
IH-honnette <74826273+honnette-code@users.noreply.github.com>
2021-06-10 11:30:47 +0200

    php

--- application/controllers/MyApp.php
+++ application/controllers/MyApp.php
@@ -197,7 +197,7 @@ public function getLoginInfo(){
 				else{
 					echo "logged in successfully";
 				}
-	}
+			}
 			}
 
 			}
--- application/views/template/view_users.php
+++ application/views/template/view_users.php
@@ -27,13 +27,15 @@
         </tr>
     </thead>
     <tbody>
-        <?php foreach ($users as $user):?>
+        <?php foreach ($users as $user):
+            $encrypt =$this->encryption->encrypt($user->userId);
+                 $encrypted=base64_encode($encrypt );?>
         <tr>
             <td><?= $user->name ?></td>
             <td><?= $user->email ?></td>
             <td><?= $user->phone ?></td>
-            <td><a href="<?= base_url('MyApp/edit_user/'.$user->userId);?>">Edit</a></td>
-            <td><a onclick="return confirm('Are you sure you want to delete this user?')" href="<?= base_url('MyApp/delete_user/'.$user->userId);?>">Remove</a></td>
+            <td><a href="<?= base_url('MyApp/edit_user/'.$encrypted);?>">Edit</a></td>
+            <td><a onclick="return confirm('Are you sure you want to delete this user?')" href="<?= base_url('MyApp/delete_user/'.$encrypted);?>">Remove</a></td>
         </tr>
         <?php endforeach;?>
     </tbody>
