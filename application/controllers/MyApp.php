ade6ea8185f5d6bf2b103b1a1ef414da1874eb3c
Yarrison allain <uwenayoallain@gmail.com>
2021-06-10 19:58:31 +0200

    initial

--- application/controllers/MyApp.php
+++ application/controllers/MyApp.php
@@ -30,48 +30,46 @@ class MyApp extends CI_Controller
 		$this->load->view('template/header');
 		$this->load->view('template/regcar');
 	}
-  
-	public function carValidation(){
+
+	public function carValidation()
+	{
 		$config['upload_path'] = './uploads/';
 		$config['allowed_types'] = 'gif|jpg|png';
 		$config['encrypt_name'] = true;
-		
-        $this->load->library('upload',$config);
+
+		$this->load->library('upload', $config);
 
 		//validation goes here
-		$this->form_validation->set_rules('name','Name','required');
-		$this->form_validation->set_rules('model','Model','required');
-		$this->form_validation->set_rules('seats','Seats','required');
-		$this->form_validation->set_rules('price','Hireprice','required');
-		$this->form_validation->set_error_delimiters('<div class="error">','</div>');
+		$this->form_validation->set_rules('name', 'Name', 'required');
+		$this->form_validation->set_rules('model', 'Model', 'required');
+		$this->form_validation->set_rules('seats', 'Seats', 'required');
+		$this->form_validation->set_rules('price', 'Hireprice', 'required');
+		$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
 
-		if($this->form_validation->run()){
-			if($this->upload->do_upload('carimage')){
-			$image_name = $this->upload->data()
-			$name = $this->input->post('name');
-			$model = $this->input->post('model');
-			$seats = $this->input->post('seats');
-			$price = $this->input->post('price');
-			$carimage = $image_name['file_name'];
+		if ($this->form_validation->run()) {
+			if ($this->upload->do_upload('carimage')) {
+				$image_name = $this->upload->data();
+				$name = $this->input->post('name');
+				$model = $this->input->post('model');
+				$seats = $this->input->post('seats');
+				$price = $this->input->post('price');
+				$carimage = $image_name['file_name'];
 
-			$data =array('name' => $name, 'model' => $model, 'seats' =>$seats ,'price'=>$price ,'carimage'=>$carimage);
-			//send the data to the model and
-			 $this->load->model('Cars');
-			 $this->Cars->insert_data($data);
-			 $this->set_flashdata('success_msg', 'New car successfully registered');
-			redirect(base_url('MyApp/index'));
-
-		}
-			else{
-			$this->set_flashdata('error_msg', 'Failed to upload image');
+				$data = array('name' => $name, 'model' => $model, 'seats' => $seats, 'price' => $price, 'carimage' => $carimage);
+				//send the data to the model and
+				$this->load->model('Cars');
+				$this->Cars->insert_data($data);
+				$this->set_flashdata('success_msg', 'New car successfully registered');
+				redirect(base_url('MyApp/index'));
+			} else {
+				$this->set_flashdata('error_msg', 'Failed to upload image');
+				$this->load->view('template/header');
+				$this->load->view('template/regcar');
+			}
+		} else {
 			$this->load->view('template/header');
 			$this->load->view('template/regcar');
 		}
-	}
-	else{
-		$this->load->view('template/header');
-		$this->load->view('template/regcar');
-	}
 	}
 
 	public function viewcars()
@@ -80,7 +78,7 @@ class MyApp extends CI_Controller
 		$data['cars'] = $this->Cars->getAll_cars();
 
 		$this->load->view('template/header');
-		$this->load->view('template/viewcars',$data);
+		$this->load->view('template/viewcars', $data);
 	}
 
 	public function isDigits(string $s, int $minDigits = 9, int $maxDigits = 14): bool
@@ -93,33 +91,34 @@ class MyApp extends CI_Controller
 			$count = 1;
 			$telephone = str_replace(['+'], '', $telephone, $count); //remove +
 		}
+	}
 
-
-		public function checkName($name){
-			if(!preg_match("/^[a-zA-Z-' ]*$/",$name)){
-				$this->form_validation->set_message('checkName','Only letters and white space are allowed for names*.');
-				return false;
-			}
-			else{
-				return true;
-			}
-
+	public function checkName($name)
+	{
+		if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+			$this->form_validation->set_message('checkName', 'Only letters and white space are allowed for names*.');
+			return false;
+		} else {
+			return true;
 		}
 	}
 	public function validateEmail()
 	{
 		$this->load->library('encryption');
+		$this->load->library('form_validation');
+		$this->form_validation->CI = &$this;
 		$this->load->library('email');
 		$this->load->helper(array('cookie', 'url'));
-		$this->form_validation->set_rules('email', 'Email', 'required|valid_email|max_length[50]');
+		$this->form_validation->set_rules('email', 'Email', 'required|valid_email|max_length[50]|callback_check_emailexist');
 		$this->form_validation->set_error_delimiters('<div class="m-2 alert-danger p-3">', '</div>');
+		$this->form_validation->set_message('check_emailexist', 'No Such User Found.');
 		if ($this->form_validation->run()) {
 			$email = $this->input->post('email');
+			$this->load->model('Users');
 			$expires = Date('U') + 3000;
 			$emailhash = $this->encryption->encrypt($email);
 			$token = bin2hex(random_bytes(20));
 			$url = base_url('MyApp/newpassword?auth=' . $emailhash . '&token=' . $token);
-
 			//SMTP & mail configuration
 			$config = array(
 				'protocol' => 'smtp',
@@ -146,9 +145,13 @@ class MyApp extends CI_Controller
 				setcookie('expires', $expires, time() + 60 * 5);
 				setcookie('token', $token, time() + 60 * 5);
 				echo "<div class='mt-5 fs-4'><div class='m-5 alert-success p-5 m-auto col-lg-6'>Email sent,check your email</div><div>";
+			} else {
+				$this->load->view('template/passwordreset');
 			}
 		} else {
-			$this->load->view('template/passwordreset');
+			$error = "<div class='alert alert-danger'>Error:No Such User Found";
+			$error .= "</div><div><a href='javascript:history.go(-1)'>Back</a></div>";
+			echo $error;
 		}
 	}
 
@@ -174,47 +177,59 @@ class MyApp extends CI_Controller
 			return true;
 		}
 	}
+	function check_emailexist($email)
+	{
+		$this->load->model('Users');
+		$query = $this->Users->gettingUser($email);
+		if ($query->num_rows() > 0) {
+			return TRUE;
+		} else {
+			return FALSE;
+		}
+	}
 
-			public function checkValildation(){
-				//validation goes here
-				$this->form_validation->set_rules('name','Name','required|min_length[5]|max_length[200]|callback_checkName');
-				$this->form_validation->set_rules('email','Email','required|valid_email|max_length[50]|is_unique[users.email]|callback_checkEmail');
-				$this->form_validation->set_rules('pswd','Password','required|min_length[6]|max_length[15]|callback_checkPassword');
-				$this->form_validation->set_rules('phone','Phone','required|min_length[10]|max_length[14]|callback_checkPhone');
-				$this->form_validation->set_rules('username','Username','required|min_length[5]|max_length[15]|is_unique[users.username]|alpha_numeric');
-				$this->form_validation->set_rules('roles','Role','required');
-				// $this->form_validation->set_rules('username','Username','required|matches[password]');
-				$this->form_validation->set_error_delimiters('<div class="error">','</div>');
-				if($this->form_validation->run())
-				{
-					$name = $this->input->post('name');
-					$email = $this->input->post('email');
-					$phone =$this->input->post('phone');
-					$pswd = $this->input->post('pswd');
-					$username = $this->input->post('username');
-					$role = $this->input->post('roles');
-					$final_pswd =hash('SHA512',$pswd);
-					$data =array('name' => $name, 'email' => $email, 'phone' =>$phone ,'password'=>$final_pswd,'username' =>$username,'roleId' =>$role);
-					//send the data to the model and
-					$this->load->model('Users');
-					if($this->Users->insert_data($data)){
-						$this->load->view('template/welcome');
-					}
-				// $this->load->view('template/header');
-				// $this->load->view('template/view_users',$data);
-			}else{
 
+	public function checkValildation()
+	{
+		//validation goes here
+		$this->form_validation->set_rules('name', 'Name', 'required|min_length[5]|max_length[200]|callback_checkName');
+		$this->form_validation->set_rules('email', 'Email', 'required|valid_email|max_length[50]|is_unique[users.email]|callback_checkEmail');
+		$this->form_validation->set_rules('pswd', 'Password', 'required|min_length[6]|max_length[15]|callback_checkPassword');
+		$this->form_validation->set_rules('phone', 'Phone', 'required|min_length[10]|max_length[14]|callback_checkPhone');
+		$this->form_validation->set_rules('username', 'Username', 'required|min_length[5]|max_length[15]|is_unique[users.username]|alpha_numeric');
+		$this->form_validation->set_rules('roles', 'Role', 'required');
+		// $this->form_validation->set_rules('username','Username','required|matches[password]');
+		$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
+		if ($this->form_validation->run()) {
+			$name = $this->input->post('name');
+			$email = $this->input->post('email');
+			$phone = $this->input->post('phone');
+			$pswd = $this->input->post('pswd');
+			$username = $this->input->post('username');
+			$role = $this->input->post('roles');
+			$final_pswd = hash('SHA512', $pswd);
+			$data = array('name' => $name, 'email' => $email, 'phone' => $phone, 'password' => $final_pswd, 'username' => $username, 'roleId' => $role);
+			//send the data to the model and
 			$this->load->model('Users');
 			if ($this->Users->insert_data($data)) {
 				$this->load->view('template/welcome');
 			}
 			// $this->load->view('template/header');
 			// $this->load->view('template/view_users',$data);
-		} else {
-			$this->load->model('Users');
-			$data['roles'] = $this->Users->get_roles();
-			$this->load->view('template/header');
-			$this->load->view('template/view_users',$data);
+			else {
+
+				$this->load->model('Users');
+				if ($this->Users->insert_data($data)) {
+					$this->load->view('template/welcome');
+				} else {
+					$this->load->model('Users');
+					$data['roles'] = $this->Users->get_roles();
+					$this->load->view('template/header');
+					$this->load->view('template/view_users', $data);
+				}
+				// $this->load->view('template/header');
+				// $this->load->view('template/view_users',$data);
+			}
 		}
 	}
 
@@ -237,7 +252,7 @@ class MyApp extends CI_Controller
 					$passwordhash = hash("SHA512", $newpassword);
 					$data = array('password' => $passwordhash);
 					$this->Users->updatebyEmail($email, $data);
-					echo "<div class='m-2 p-2 alert-success' >Success</div>";
+					echo "<div class='m-2 p-2 alert-success' >Success</a>";
 				} else {
 					$this->load->view('template/newpassword');
 				}
@@ -289,49 +304,46 @@ class MyApp extends CI_Controller
 			$pswd = $this->input->post('pswd');
 			$hashedPassword = hash("SHA512", $pswd);
 			$this->load->model("Users");
-		    $user=$this->Users->gettingUser($email);
-			if(!$user){
+			$user = $this->Users->gettingUser($email);
+			if (!$user) {
 				echo "no user found";
-			}
-			else{
+			} else {
 				foreach ($user->result() as $row) {
-				$userPass = $row->password;
-				if($hashedPassword!==$userPass){
-					echo "invalid email or password";
-				}
-				else{
+					$userPass = $row->password;
+					if ($hashedPassword !== $userPass) {
+						echo "invalid email or password";
+					} else {
 						$this->load->view('template/viewcars');
+					}
 				}
 			}
-			}
-
-}
-else{
-$this->load->view('template/header');
-$this->load->view('template/login');
-}
-}
-			public function edit_user(){
-			$id =$this->uri->segment(3);
-			   $this->load->model('Users');
-			   $data['users']=$this->Users->get_user($id);
-				 //return the form
-			   $this->load->view('template/header');
-			   $this->load->view('template/edit_data',$data); 
-			   }
-			   public function edit_record(){
-				$id =$this->uri->segment(3);
-				$name = $this->input->post('name');
-					$email = $this->input->post('email');
-					$phone =$this->input->post('phone');
-					$username = $this->input->post('username');
-					$data =array('name' => $name, 'email' => $email, 'phone' =>$phone ,'username' =>$username);
-					//send the data to the model and
-					 $this->load->model('Users');
-					if( $this->Users->update_data($id,$data)){
-						echo "<script>alert('User Updated');window.location.href=
-				'".base_url('MyApp/users')."';</script>";
-					}
-			}
-
+		} else {
+			$this->load->view('template/header');
+			$this->load->view('template/login');
+		}
+	}
+	public function edit_user()
+	{
+		$id = $this->uri->segment(3);
+		$this->load->model('Users');
+		$data['users'] = $this->Users->get_user($id);
+		//return the form
+		$this->load->view('template/header');
+		$this->load->view('template/edit_data', $data);
+	}
+	public function edit_record()
+	{
+		$id = $this->uri->segment(3);
+		$name = $this->input->post('name');
+		$email = $this->input->post('email');
+		$phone = $this->input->post('phone');
+		$username = $this->input->post('username');
+		$data = array('name' => $name, 'email' => $email, 'phone' => $phone, 'username' => $username);
+		//send the data to the model and
+		$this->load->model('Users');
+		if ($this->Users->update_data($id, $data)) {
+			echo "<script>alert('User Updated');window.location.href=
+				'" . base_url('MyApp/users') . "';</script>";
+		}
+	}
 }
--- application/models/Users.php
+++ application/models/Users.php
@@ -1,34 +1,43 @@
 <?php
-class Users extends CI_Model{
-    public function insert_data($data){
-//sql query to insert_data
-      return  $this->db->insert('users',$data);//table name
-    }
-    public function getAll_users(){
-        //$this->db->order_by('userId','ASC');
-      return  $this->db->get('users')->result();
-    }
-    public function get_roles(){
-        //$this->db->order_by('userId','ASC');
-      return $this->db->get('roles')->result();
-    }
-    public function delete_user($id){
-        return $this->db->delete('users',array('userId' => $id)); 
-    }
-    public function get_user($id){
-       return $this->db->get_where('users',array('userId' => $id))->result();
-    }
-
-    public function update_data($id,$data){
-        $this->db->where('userId',$id);
-        return $this->db->update('users',$data);
-    }
-    public function updatebyEmail($email,$data){
-      $this->db->where('email',$email);
-      return $this->db->update('users',$data);
+class Users extends CI_Model
+{
+  public function insert_data($data)
+  {
+    //sql query to insert_data
+    return  $this->db->insert('users', $data); //table name
+  }
+  public function getAll_users()
+  {
+    //$this->db->order_by('userId','ASC');
+    return  $this->db->get('users')->result();
+  }
+  public function get_roles()
+  {
+    //$this->db->order_by('userId','ASC');
+    return $this->db->get('roles')->result();
+  }
+  public function delete_user($id)
+  {
+    return $this->db->delete('users', array('userId' => $id));
+  }
+  public function get_user($id)
+  {
+    return $this->db->get_where('users', array('userId' => $id))->result();
   }
 
-        public function gettingUser($email){
-        return $this->db->get_where('users',array('email' => $email));
-        }
+  public function update_data($id, $data)
+  {
+    $this->db->where('userId', $id);
+    return $this->db->update('users', $data);
+  }
+  public function updatebyEmail($email, $data)
+  {
+    $this->db->where('users', array('email' => $email));
+    return $this->db->update('users', $data);
+  }
+
+  public function gettingUser($email)
+  {
+    return $this->db->get_where('users', array('email' => $email));
+  }
 }
