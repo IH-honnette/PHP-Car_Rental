c725c3c7b7aa15e1cf5aa7f43632a8e690d07fa4
Shumbusho43 <davidshumbusho10@gmail.com>
2021-06-14 16:55:28 +0200

    fixed honnette's errors

--- application/controllers/MyApp.php
+++ application/controllers/MyApp.php
@@ -32,9 +32,11 @@ class MyApp extends CI_Controller
 	}
 
 	public function hirecar()
-	{
+	{	
+		$this->load->model('Cars');
+		$data['cars_info'] = $this->Cars->get_cars_nothired();
 		$this->load->view('template/header');
-		$this->load->view('template/hirecar');
+		$this->load->view('template/hirecar',$data);
 	}
 	public function isValidTelephoneNumber(string $telephone, int $minDigits = 9, int $maxDigits = 14): bool {
 				if (preg_match('/^[+][0-9]/', $telephone)) { //is the first character + followed by a digit
@@ -57,40 +59,25 @@ class MyApp extends CI_Controller
 		}
 	}
 
-	public function hireValidation()
+public function hireValidation()
 	{
 		//validation goes here
-		$this->form_validation->set_rules('name', 'Name', 'required|min_length[5]|max_length[200]|callback_checkName');
-		$this->form_validation->set_rules('email', 'Email', 'required|valid_email|max_length[50]|is_unique[users.email]|callback_checkEmail');
-		$this->form_validation->set_rules('pswd', 'Password', 'required|min_length[6]|max_length[15]|callback_checkPassword');
-		$this->form_validation->set_rules('phone', 'Phone', 'required|min_length[10]|max_length[14]|callback_checkPhone');
-		$this->form_validation->set_rules('username', 'Username', 'required|min_length[5]|max_length[15]|is_unique[users.username]|alpha_numeric');
-		$this->form_validation->set_rules('roles', 'Role', 'required');
-		// $this->form_validation->set_rules('username','Username','required|matches[password]');
-		$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
-		if ($this->form_validation->run()) {
-			$name = $this->input->post('name');
-			$email = $this->input->post('email');
-			$phone = $this->input->post('phone');
-			$pswd = $this->input->post('pswd');
-			$username = $this->input->post('username');
-			$role = $this->input->post('roles');
-			$final_pswd = hash('SHA512', $pswd);
-			$data = array('name' => $name, 'email' => $email, 'phone' => $phone, 'password' => $final_pswd, 'username' => $username, 'roleId' => $role);
-			//send the data to the model and
-			$this->load->model('Users');
-			if ($this->Users->insert_data($data)) {
-				$this->load->view('template/welcome');
+		$this->form_validation->set_rules('car', 'Car', 'required');
+$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
 
-				// $this->load->view('template/header');
-				// $this->load->view('template/view_users',$data);
-			} else {
-				$this->load->model('Users');
-				$data['roles'] = $this->Users->get_roles();
-				$this->load->view('template/header');
-				$this->load->view('template/view_users', $data);
-			}
-		}
+		if ($this->form_validation->run()) {
+			$car = $this->input->post('car');
+			$data = array('hired' => True);
+$this->load->model('Cars');
+			if ($this->Cars->update_car($car, $data)) {
+				$this->session->set_flashdata('success_msg', 'Successfully hired a car');
+				redirect(base_url('MyApp/hired'));
+}}}
+
+public function hired()
+	{
+		$this->load->view('template/header');
+		$this->load->view('template/hired');
 	}
 
 	public function carValidation()
@@ -407,7 +394,7 @@ class MyApp extends CI_Controller
 			$error = "invalid email or password";
 			$this->load->view('template/login', compact('error'));
 			// $this->session->set_flashdata('error','invalid email or password');
-			redirect(base_url('MyApp/login'));
+			// redirect(base_url('MyApp/login'));
 		} else {
 			$this->load->view('template/header');
 			$this->load->view('template/login');
